e314eb89c189eeaae1334c7987cae6c5dda641ec
kylek44 <kylek44@outlook.com>
2021-08-30 12:38:07 -0500

    Fix reload.js

--- reload.js
+++ reload.js
@@ -14,6 +14,7 @@ sequelize.sync();
 client.on('ready', async () => {
   const guild = await client.guilds.fetch(process.env.BOT_GUILD_ID);
   const users = await guild.members.fetch();
+  console.log(users);
   for (const user of users) {
     if (!user.roles.cache.has(process.env.BOT_VERIFIED_ID)) continue;
     const u = await User.findOne({

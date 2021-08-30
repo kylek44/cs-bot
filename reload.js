ac9954a5ffb401a256f81dc0cb61a7eab5044b59
kylek44 <kylek44@outlook.com>
2021-08-30 12:34:21 -0500

    Fix reload.js

--- reload.js
+++ reload.js
@@ -15,6 +15,7 @@ client.on('ready', async () => {
   const guild = await client.guilds.fetch(process.env.BOT_GUILD_ID);
   const users = await guild.members.fetch();
   for (const user of users) {
+    if (!user.roles.cache.has(process.env.BOT_VERIFIED_ID)) continue;
     const u = await User.findOne({
       where: {
         snowflake: user.id
